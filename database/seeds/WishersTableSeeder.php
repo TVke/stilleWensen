f0bb66839dcc1feae513da22603cdfe91d641d39
TVke <thomas.verhelst@student.kdg.be>
2017-12-19 03:07:56 +0100

    fixed seeding error

--- database/seeds/WishersTableSeeder.php
+++ database/seeds/WishersTableSeeder.php
@@ -13,7 +13,7 @@ class WishersTableSeeder extends Seeder
     public function run()
     {
         DB::table('wishers')->insert([
-            ['sender_name' => 'Thomas Verhelst','sender_email' => 'tvke91@gmail.com','quit_slug' => str_slug('Thomas Verhelst'),'recipient_name' => 'Phedra','recipient_email' => 'tvke91@gmail.com','full_sound_slug' => str_slug('Thomas Verhelst'),'soundless_video' => null,'sound_video' => null,'allow_public' => 0,],
+            ['sender_name' => 'Thomas Verhelst','sender_email' => 'tvke91@gmail.com','quit_slug' => str_slug('Thomas Verhelst'),'recipient_name' => 'Phedra','recipient_email' => 'tvke91@gmail.com','full_sound_slug' => str_slug('Thomas Verhelst'),'soundless_video' => null,'sound_video' => null,'allow_public' => 0,'sound_available' => '2017-12-19 03:07:08',],
         ]);
     }
 }
