cfcf0f49f646ff2501209ab0dc651e820228f882
TVke <thomas.verhelst@student.kdg.be>
2017-12-20 12:18:47 +0100

    added contact validation and saving to DB

---
+++ app/Mail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace stilleWensen;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mail extends Model
+{
+    protected $fillable = ['from_name','from_email','subject','content'];
+}
--- resources/views/wisher/add.blade.php
+++ resources/views/wisher/add.blade.php
@@ -11,15 +11,15 @@
 		<section class="flex flex-wrap justify-between max-w-lg mx-auto px-8">
 			<section class="w-64">
 				<label for="sender_name" class="text-left block mt-8">{{ __('app.name-sender') }}</label>
-				<input type="text" name="sender_name" id="sender_name" class="inputfield">
+				<input type="text" name="sender_name" id="sender_name" class="inputfield" required>
 				<label for="sender_email" class="text-left block mt-8">{{ __('app.email-sender') }}</label>
-				<input type="text" name="sender_email" id="sender_email" class="inputfield">
+				<input type="email" name="sender_email" id="sender_email" class="inputfield" required>
 			</section>
 			<section class="w-64">
 				<label for="recipient_name" class="text-left block mt-8">{{ __('app.name-receiver') }}</label>
 				<input type="text" name="recipient_name" id="recipient_name" class="inputfield">
 				<label for="recipient_email" class="text-left block mt-8">{{ __('app.email-receiver') }}</label>
-				<input type="text" name="recipient_email" id="recipient_email" class="inputfield">
+				<input type="email" name="recipient_email" id="recipient_email" class="inputfield">
 			</section>
 		</section>
 		<label for="allow_public" class="text-left block max-w-lg mx-auto p-8"><input type="checkbox" class="mr-2" name="allow_public" id="allow_public"> {{ __('app.form-public') }}</label>
