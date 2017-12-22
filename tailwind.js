a8acaf5d20ec6aadaf78d97baded50445633b862
TVke <thomas.verhelst@student.kdg.be>
2017-12-22 03:01:16 +0100

    added correct green for validation

--- tailwind.js
+++ tailwind.js
@@ -84,7 +84,7 @@ var colors = {
     'green-darkest': '#0f2f21',
     'green-darker': '#1a4731',
     'green-dark': '#1f9d55',
-    'green': '#38c172',
+    'green': '#5ce44f',
     'green-light': '#51d88a',
     'green-lighter': '#a2f5bf',
     'green-lightest': '#e3fcec',
