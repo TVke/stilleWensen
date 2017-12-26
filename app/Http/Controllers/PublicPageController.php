1b20f46ae4c8290493735b382c218375c5245cda
TVke <thomas.verhelst@student.kdg.be>
2017-12-26 19:48:15 +0100

    fixed background color in some mailclients

--- app/Http/Controllers/PublicPageController.php
+++ app/Http/Controllers/PublicPageController.php
@@ -114,7 +114,7 @@ class PublicPageController extends Controller
             <div style='width:100%;display:inline-block;vertical-align:top;'>
                 <table width='100%' cellspacing='0' cellpadding='0' border='0'>
                     <tbody>
-                    <tr>
+                    <tr bgcolor='#011937'>
                         <td style=\"font-size:16px;font-family:'Dosis',Helvetica,Arial,sans-serif;\">
 
 
@@ -131,7 +131,7 @@ class PublicPageController extends Controller
                             <a href=\"https://stillewensen.be/\">
                                 <table width='100%' align='center' cellpadding='0' cellspacing='0' border='0'>
                                     <tbody>
-                                    <tr>
+                                    <tr  bgcolor='#011937'>
                                         <td>
                                             <img src=\"https://stillewensen.be/storage/img/logo.png\" alt='Stille Wensen' width='128px' height='78px' style='display:block;margin:0 auto;width:128px;color:#c2c1c1;'>
                                         </td>
@@ -147,7 +147,7 @@ class PublicPageController extends Controller
 
                             <table width='100%' align='center' cellpadding='0' cellspacing='0' border='0' style='padding:20px;'>
                                 <tbody>
-                                <tr>
+                                <tr  bgcolor='#011937'>
                                     <td style='color:#c2c1c1;text-align:center;'>
                                         Bedankt voor je deelname!
                                     </td>
@@ -161,7 +161,7 @@ class PublicPageController extends Controller
 
                             <table width='100%' align='center' cellpadding='0' cellspacing='0' border='0'>
                                 <tbody>
-                                <tr>
+                                <tr bgcolor='#011937'>
                                     <td style='color:#c2c1c1;text-align:center;'>
                                         <a bgcolor='#247c79' width='150px' href=\"https://stillewensen.be/video/".$wish->quiet_slug."/".$wish->full_sound_slug."\" style='display:block;width:150px;margin:10px auto;color:#c2c1c1;background:#247c79;padding:10px 0;text-decoration:none;'>Je video met geluid</a>
                                     </td>
@@ -201,7 +201,7 @@ class PublicPageController extends Controller
 
         if(mail(
             'tvke91@gmail.com',
-            "Stille Wensen video :".$wish->sender_email,
+            "Stille Wensen video",
             $HTMLcontent,
             "From: contact@stillewensen.be\r\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=ISO-8859-1\r\n"
         )){
