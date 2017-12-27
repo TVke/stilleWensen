2812890be00b30ca08845d010a054ba959c3af3b
TVke <thomas.verhelst@student.kdg.be>
2017-12-27 16:44:55 +0100

    implemented Phedra's comments only color is not official

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,7 +2,7 @@
 @section('title', $wisher->sender_name.' - ')
 @section('content')
 	<section class="max-w-lg mx-auto px-4">
-		<h2 class="pb-8">{!! starToElement(__('app.wishes-title'),"span") !!}</h2>
+		<h2 class="text-3xl pt-16 pb-8">{!! starToElement(__('app.wishes-title'),"span") !!}</h2>
 		<p class="pb-4">{!! starToElement(__('app.wishes-info'),"strong") !!}</p>
         @if($wisher->recipient_name)
             <p class="pb-8">{!! starToElement(__('app.wishes-personal',['Sender' => $wisher->sender_name,'Reciever' => $wisher->recipient_name]),"strong") !!}</p>
